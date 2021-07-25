e454f593a11f6735469349a3b9d0b50c074622bf
Kevin Bungeneers <kevin@bungerous.be>
2021-07-25 17:18:59 +0200

    Fixed typo in hammerspoon config

--- nixpkgs/home/hammerspoon/default.nix
+++ nixpkgs/home/hammerspoon/default.nix
@@ -3,7 +3,7 @@
 {
     # I only use home-manager to keep my hammerspoon config in this repo as it's easier to manage and because
     # Hammerspoon has no support for the .config directory.
-    # Actual modifications to my hammerspoon configuration is done through the HS application.
+    # Actual modifications to my hammerspoon configuration are done through the HS application.
     home.file.".hammerspoon/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
     home.file.".hammerspoon/Spoons".source = config.lib.file.mkOutOfStoreSymlink ./Spoons;
 }
