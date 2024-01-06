70f4d7e800c264df74e8b49f2d321183d1fe73fb
Kevin Bungeneers <kevin@bungerous.be>
2024-01-07 00:24:27 +0100

    Removed Hammerspoon configuration

--- home-manager/hammerspoon/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-    # I only use home-manager to keep my hammerspoon config in this repo as it's easier to manage and because
-    # Hammerspoon has no support for the .config directory.
-    # Actual modifications to my hammerspoon configuration are done through the HS application.
-    home.file.".hammerspoon/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
-    home.file.".hammerspoon/Spoons".source = config.lib.file.mkOutOfStoreSymlink ./Spoons;
-}
